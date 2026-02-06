aa38d8a8868c485af654ef20cf4b09d59bcd5891
erenyeager283 <vtu24922@veltech.edu.in>
2026-02-06 10:05:59 +0530

    Time: 61 ms (67.55%), Space: 108 MB (7.24%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // Pop all previous colder days
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            // Push current day index
+            stack.push(i);
+        }
+        return answer;
+    }
+}
